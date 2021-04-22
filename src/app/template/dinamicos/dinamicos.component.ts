ef1a50ea21029928ada177c65d2a792425f04710
masset151 <masset.ruiz151@gmail.com>
2021-04-22 18:24:16 +0200

    Reactive

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona{
+  nombre:string,
+  favoritos:favoritos[]
+}
+
+interface favoritos{
+  id:number,
+  nombre:string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
 @ViewChild('miFormulario') formulario!: NgForm
-  constructor() { }
 
-  ngOnInit(): void {
+persona:Persona = {
+nombre:'Andres',
+favoritos:[
+  {
+    id:1,
+    nombre:'Fifa 21'
+  },
+
+  {
+    id:2,
+    nombre:'Grand Thef Auto V'
   }
+
+]
+}
 
 
   validar():boolean{
-    return this.formulario.controls.nombre?.valid && this.formulario.controls.nombre?.touched
+    return this.formulario?.controls.nombre?.touched && this.formulario.controls.nombre.invalid
+    
   }
 
   guardar(){
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
   }
+
+  terminosYCondiciones: boolean = false;
+
 
 }
