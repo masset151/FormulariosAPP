086f0ddf44d9f1b438397e8c88999b5dcaf0e2a7
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-04-23 09:17:23 +0200

    reparacion

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -5,6 +5,7 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    FormsModule
   ]
 })
 export class ReactiveModule { }
