4a3618bc42394d12e0df36b780f126bc454237b6
masset151 <masset.ruiz151@gmail.com>
2021-04-27 17:31:11 +0200

    Mensajes de error personalizados

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,3 +1,4 @@
+import { getParseErrors } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
@@ -15,13 +16,34 @@ export class RegistroComponent implements OnInit {
 
     miFormulario:FormGroup = this.fb.group({
     nombre:['',[Validators.required, Validators.pattern(this.validatorService.nombreApellidoPatter)]],
-    email: ['',[Validators.required,Validators.email,Validators.pattern(this.validatorService.emailPattern)],[this.emailValidator.validate]],  
+    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ], [ this.emailValidator ] ],
     username: ['',[Validators.required,this.validatorService.noPuedeSerStrider]],
     password:['',[Validators.required,Validators.minLength(6)]],
     password2:['',[Validators.required]]
   },{
     Validators:[this.validatorService.camposIguales('password','password2')]
   })
+
+  
+  
+  
+get emailErrorMsg():string{
+  const error = this.miFormulario.get('email')?.errors
+
+  if(error?.required){
+    return 'El email es obligatorio'
+  }
+
+  if(error?.pattern){
+    return 'el email introducido no es valido'
+  }
+
+  if(error?.emailTomado){
+    return 'El correo ya existe, inicie sesion'
+  }
+
+  return ''
+}
 
   constructor(private fb:FormBuilder, private validatorService:ValidatorsService,private emailValidator:EmailValidatorService){}
 
@@ -31,7 +53,9 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.reset({
       nombre:"Andres Masset",
       email:"test1@test.com",
-      username:'masset_151'
+      username:'masset_151',
+      password:'123456',
+      password2:'123456'
     })
     
   }
@@ -45,7 +69,7 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.markAllAsTouched();
   }
 
-
+  
 
 
   
